a260b52a3beea20f73b13e0d5da765bd7d032a35
paplouand-spec <paplouand@gmail.com>
2026-03-16 14:52:34 +0000

    b

--- PAP-Boleibol-2D/Assets/Scripts/SettingsManager.cs
+++ PAP-Boleibol-2D/Assets/Scripts/SettingsManager.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class SettingsManager : MonoBehaviour
+public class SettingsMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Toggle fullscreenToggle;
+
     void Start()
     {
-        
+        // Carrega a preferência de fullscreen guardada ou define como true por padrão
+        if (PlayerPrefs.HasKey("fullscreen"))
+        {
+            fullscreenToggle.isOn = PlayerPrefs.GetInt("fullscreen") == 1 ? true : false;
+        }
+        else
+        {
+            fullscreenToggle.isOn = Screen.fullScreen;
+        }
+        SetFullscreen(fullscreenToggle.isOn);
+
+        // Adiciona um listener para quando o valor do Toggle muda
+        fullscreenToggle.onValueChanged.AddListener(delegate { SetFullscreen(fullscreenToggle.isOn); });
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetFullscreen(bool isFullscreen)
     {
-        
+        Screen.fullScreen = isFullscreen;
+        PlayerPrefs.SetInt("fullscreen", isFullscreen ? 1 : 0);
+        PlayerPrefs.Save();
     }
 }
